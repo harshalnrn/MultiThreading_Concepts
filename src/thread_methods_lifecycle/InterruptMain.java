89e25948e4ff5015304207ace3c103152600d827
Harshal <harshalnrn>
2021-04-27 14:05:31 +0530

    testing commit

--- src/thread_methods_lifecycle/InterruptMain.java
+++ src/thread_methods_lifecycle/InterruptMain.java
@@ -5,7 +5,7 @@ public class InterruptMain {
         Thread th = new InterruptingThreadExample();
         th.start();
 
-th.interrupt(); // note: we can only interrupt a thread thats in sleep mode
+th.interrupt(); // note: we can only interrupt a thread thats in sleep mode/wait mode .
              // Thus here only 1 itiration in child thread run(), since it goes to sleep after each itiration, and gets interrupted in very first itiration
         System.out.println("end of main method");
     }
